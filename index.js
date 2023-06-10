d1e1dfe9b468d47427cfab0ec0f24d3dcbac8da6
MsEmily1020 <peace10200khs@gmail.com>
2023-06-10 20:43:54 +0900

    [Fix] 클릭리스너 에러 해결

--- index.js
+++ index.js
@@ -32,7 +32,25 @@ const conversions = {
 };
 
 // 결과값 계산 후 리턴
-function convertToValue(inputType, inputText, outputType) {
+function convertToValue(inputSelector, inputText, outputSelector) {
+  const inputType = inputSelector.value;
+  const outputType = outputSelector.value;
+
+  const value = parseInt(inputText.value);
+  if (isNaN(value)) {
+    return "Invalid input";
+  }
+
   const conversionFunction = conversions[inputType]?.[outputType];
-  return conversionFunction ? "${conversionFunction(value)}" : "Invalid input";
+  return conversionFunction
+    ? `${conversionFunction(value)} ${outputType}`
+    : "Invalid input";
 }
+
+// 클릭리스너 구현
+function addEvent() {
+  outputText.value = convertToValue(inputSelector, inputText, outputSelector);
+}
+
+inputText.addEventListener("change", addEvent);
+inputSelector.addEventListener("change", addEvent);
