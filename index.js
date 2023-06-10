6fc6ea99281a79d2986fc59360a2f648731d65d8
MsEmily1020 <peace10200khs@gmail.com>
2023-06-10 17:52:03 +0900

    [Feat] form 요소 가져오기

---
+++ index.js
@@ -0,0 +1,5 @@
+// form 요소 가져오기
+const input_selector = document.getElementById("input_selector");
+const input_text = document.getElementById("input_text");
+const output_selector = document.getElementById("output_selector");
+const output_text = document.getElementById("output_text");
