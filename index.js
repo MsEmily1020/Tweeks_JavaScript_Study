b249c53e44d266918cc0c7b0a321dd2c01fea252
MsEmily1020 <peace10200khs@gmail.com>
2023-06-10 20:14:32 +0900

    [Feat] bit, byte, kb, mb 구분 & 객체화

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 // form 요소 가져오기
-const input_selector = document.getElementById("input_selector");
-const input_text = document.getElementById("input_text");
-const output_selector = document.getElementById("output_selector");
-const output_text = document.getElementById("output_text");
+const inputSelector = document.getElementById("input_selector");
+const inputText = document.getElementById("input_text");
+const outputSelector = document.getElementById("output_selector");
+const outputText = document.getElementById("output_text");
+
+// 전환 객체
+const conversions = {
+  bit: {
+    byte: (value) => value / 8,
+    kb: (value) => value / (1024 * 8),
+    mb: (value) => value / (1024 * 1024 * 8),
+  },
+
+  byte: {
+    bit: (value) => value * 8,
+    kb: (value) => value / 1024,
+    mb: (value) => value / (1024 * 1024),
+  },
+
+  kb: {
+    bit: (value) => value * 8 * 1024,
+    byte: (value) => value * 1024,
+    mb: (value) => value / 1024,
+  },
+
+  mb: {
+    bit: (value) => value * 8 * 1024 * 1024,
+    byte: (value) => value * 1024 * 1024,
+    kb: (value) => value * 1024,
+  },
+};
